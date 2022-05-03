1b690dea372b257f6eafa56a15bbfbf073c43042
Marcus Gelderman <marcgeld@gmail.com>
2022-05-04 00:10:06 +0200

    refactoring

--- plugins/inputs/airthings/airthings_test.go
+++ plugins/inputs/airthings/airthings_test.go
@@ -20,6 +20,25 @@ const (
 	HttpContentTypeKey  = "Content-Type"
 	HttpContentTypeForm = "application/x-www-form-urlencoded"
 	HttpContentTypeJson = "application/json"
+
+	MesBattery           = "battery"
+	MesHumidity          = "humidity"
+	MesMold              = "mold"
+	MesRelayDeviceType   = "relayDeviceType"
+	MesRssi              = "rssi"
+	MesTemp              = "temp"
+	MesVoc               = "voc"
+	MesRadonShortTermAvg = "radonShortTermAvg"
+	MesCo2               = "co2"
+	MesPressure          = "pressure"
+
+	TagName           = "name"
+	TagId             = "id"
+	TagDeviceType     = "deviceType"
+	TagSegmentId      = "segment.id"
+	TagSegmentName    = "segment.name"
+	TagSegmentActive  = "segment.active"
+	TagSegmentStarted = "segment.started"
 )
 
 func readTestData(testdataFilename string) string {
@@ -83,78 +102,91 @@ func TestGetDeviceListAndData(t *testing.T) {
 	err := acc.GatherError(airthings.Gather)
 	require.NoError(t, err)
 
-	acc.AssertContainsTaggedFields(t, "airthings_connector",
-		map[string]interface{}{
-			"battery":         float64(78),
-			"humidity":        float64(24),
-			"mold":            float64(0),
-			"relayDeviceType": "hub",
-			"rssi":            float64(-51),
-			"temp":            float64(22.9),
-			"voc":             float64(161),
-		},
-		map[string]string{
-			"name":            "airthings",
-			"id":              "9990019182",
-			"deviceType":      "WAVE_MINI",
-			"segment.id":      "c6ddc7f5-e052-4969-8cca-f79f6a96b4f1",
-			"segment.name":    "VOC",
-			"segment.active":  "true",
-			"segment.started": "2120-09-12T07:20:28",
-		})
+	assertWaveMini(t, acc)
+	assertWavePlus(t, acc)
+	assertGen2(t, acc)
+	assertHub(t, acc)
+}
 
+func assertWaveMini(t *testing.T, acc testutil.Accumulator) {
 	acc.AssertContainsTaggedFields(t, "airthings_connector",
 		map[string]interface{}{
-			"battery":           float64(100),
-			"co2":               float64(1456),
-			"humidity":          float64(41),
-			"pressure":          float64(1000.7),
-			"radonShortTermAvg": float64(92),
-			"relayDeviceType":   "hub",
-			"rssi":              float64(-64),
-			"temp":              float64(19.4),
-			"voc":               float64(191),
+			MesBattery:         float64(78),
+			MesHumidity:        float64(24),
+			MesMold:            float64(0),
+			MesRelayDeviceType: "hub",
+			MesRssi:            float64(-51),
+			MesTemp:            float64(22.9),
+			MesVoc:             float64(161),
 		},
 		map[string]string{
-			"deviceType":      "WAVE_PLUS",
-			"id":              "9990131459",
-			"name":            "airthings",
-			"segment.active":  "true",
-			"segment.id":      "2bd162ce-4470-429f-8eff-4680ed5c6197",
-			"segment.name":    "Bedroom",
-			"segment.started": "2122-10-22T20:19:18",
+			TagName:           "airthings",
+			TagId:             "9990019182",
+			TagDeviceType:     "WAVE_MINI",
+			TagSegmentId:      "c6ddc7f5-e052-4969-8cca-f79f6a96b4f1",
+			TagSegmentName:    "VOC",
+			TagSegmentActive:  "true",
+			TagSegmentStarted: "2120-09-12T07:20:28",
 		})
+}
 
+func assertWavePlus(t *testing.T, acc testutil.Accumulator) {
 	acc.AssertContainsTaggedFields(t, "airthings_connector",
 		map[string]interface{}{
-			"battery":           float64(100),
-			"humidity":          float64(23),
-			"radonShortTermAvg": float64(165),
-			"relayDeviceType":   "hub",
-			"rssi":              float64(-59),
-			"temp":              float64(23.3),
+			MesBattery:           float64(100),
+			MesCo2:               float64(1456),
+			MesHumidity:          float64(41),
+			MesPressure:          float64(1000.7),
+			MesRadonShortTermAvg: float64(92),
+			MesRelayDeviceType:   "hub",
+			MesRssi:              float64(-64),
+			MesTemp:              float64(19.4),
+			MesVoc:               float64(191),
 		},
 		map[string]string{
-			"deviceType":      "WAVE_GEN2",
-			"id":              "9990012993",
-			"name":            "airthings",
-			"segment.active":  "true",
-			"segment.id":      "3f2f2e23-f81d-46dd-8da6-9c5ed051b6e5",
-			"segment.name":    "Basement",
-			"segment.started": "2122-11-11T17:52:43",
+			TagDeviceType:     "WAVE_PLUS",
+			TagId:             "9990131459",
+			TagName:           "airthings",
+			TagSegmentActive:  "true",
+			TagSegmentId:      "2bd162ce-4470-429f-8eff-4680ed5c6197",
+			TagSegmentName:    "Bedroom",
+			TagSegmentStarted: "2122-10-22T20:19:18",
 		})
+}
 
+func assertGen2(t *testing.T, acc testutil.Accumulator) {
 	acc.AssertContainsTaggedFields(t, "airthings_connector",
 		map[string]interface{}{
-			"battery": "N/A",
+			MesBattery:           float64(100),
+			MesHumidity:          float64(23),
+			MesRadonShortTermAvg: float64(165),
+			MesRelayDeviceType:   "hub",
+			MesRssi:              float64(-59),
+			MesTemp:              float64(23.3),
 		},
 		map[string]string{
-			"deviceType":      "HUB",
-			"id":              "9990002665",
-			"name":            "airthings",
-			"segment.active":  "true",
-			"segment.id":      "68e384b9-129c-4b41-9fc3-66af3d80e7b6",
-			"segment.name":    "AirthingsHub",
-			"segment.started": "2120-10-27T12:29:28",
+			TagDeviceType:     "WAVE_GEN2",
+			TagId:             "9990012993",
+			TagName:           "airthings",
+			TagSegmentActive:  "true",
+			TagSegmentId:      "3f2f2e23-f81d-46dd-8da6-9c5ed051b6e5",
+			TagSegmentName:    "Basement",
+			TagSegmentStarted: "2122-11-11T17:52:43",
+		})
+}
+
+func assertHub(t *testing.T, acc testutil.Accumulator) {
+	acc.AssertContainsTaggedFields(t, "airthings_connector",
+		map[string]interface{}{
+			MesBattery: "N/A",
+		},
+		map[string]string{
+			TagDeviceType:     "HUB",
+			TagId:             "9990002665",
+			TagName:           "airthings",
+			TagSegmentActive:  "true",
+			TagSegmentId:      "68e384b9-129c-4b41-9fc3-66af3d80e7b6",
+			TagSegmentName:    "AirthingsHub",
+			TagSegmentStarted: "2120-10-27T12:29:28",
 		})
 }
